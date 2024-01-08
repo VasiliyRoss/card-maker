58f7118ce5989cf33f07a26098a27159d569b0a8
Vasiliy Krestyaninov <vasiliy.krestyaninov@ispring.com>
2024-01-08 03:12:04 +0300

    подключен redux

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,30 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styles from './Card.module.css';
 import { CardData } from '../../types/content';
 import Canvas from '../Canvas/Canvas';
 
 function Card({ canvas, filter }: CardData) {
-    const canvasSize = canvas.size;
-    const [size, setSize] = useState(canvasSize);
-
-    useEffect(() => {
-        setSize(size);
-    }, []);
-
-    const newSizeFromWebhook = {
-        height: 1000,
-        width: 1000,
-    };
-
     return (
         <div className={styles.wrapper}>
             <div
                 className={styles.filter}
                 style={{ background: filter?.color }}
             ></div>
-            <button onClick={() => setSize(newSizeFromWebhook)}>
-                NEW SIZE HERE
-            </button>
             <Canvas
                 size={canvas.size}
                 objects={canvas.objects}
